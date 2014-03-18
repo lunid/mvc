b00bda06043c01810b924804475d6a2042408d90
Claudio <Claudio@Claudio-PC>
2014-03-18 17:48:55 -0300

    Load mail.

--- teste/load_mail.php
+++ teste/load_mail.php
@@ -1,8 +1,10 @@
 <?php
     $server = 'pop.supervip.com.br';
     $port   = '110';
-    $user   = 'claudio@supervip.com.br';
-    $passwd = 'adm91100x';
+    $user   = 'project@supervip.com.br';
+    $passwd = 'senha3040';
+    
+    $arrPseudoCod = array('title','tt','ds','task','tk','tasks','tks','type','tp','deadline','dl','tag');
     
     $mbox = imap_open("{{$server}:{$port}/pop3/novalidate-cert}INBOX", $user, $passwd);
     if ($mbox) {
@@ -12,11 +14,71 @@
             $message['subject']     = $header->subject;
             $message['fromaddress'] = $header->fromaddress;
             $message['toaddress']   = $header->toaddress;
-            $message['ccaddress']   = $header->ccaddress;
+            $message['ccaddress']   = (isset($header->ccaddress)) ? $header->ccaddress : '';
             $message['date']        = $header->date;
             $message['body']        = imap_fetchbody($mbox,$msgId,"1"); ## GET THE BODY OF MULTI-PART MESSAGE
             if(!$message['body']) {$message['body'] = '[Nenhuma mensagem foi enviada]\n\n';}
-            echo  imap_qprint($message['body']);
+            
+            $titulo     = utf8_decode(iconv_mime_decode($message['subject'],0,"UTF-8"));           
+            $msg        = imap_qprint($message['body']);
+            $arrMsgTk   = $arrMsg = explode("\n",$msg);                 
+            //$arrMsg     = array_filter($arrMsgTk, "delLinhaVazia");
+            //$arrMsg     = array_values($arrMsg);
+            $arrTag     = array();
+            $arrTask    = array();
+            
+            if (is_array($arrMsg)) {
+                $tam = count($arrMsg);
+  
+                for($i=0; $i < $tam; $i++) {
+                    $line = trim($arrMsg[$i]);
+                    foreach($arrPseudoCod as $cod) {
+                        $codLang = '#'.$cod.':';
+                        
+                        $key = strpos($line,$codLang);
+                        if ($key !== false) {
+                           //Encontrou uma tag na linha atual
+                          //echo $codLang. $line.'...<br>';                           
+                          $fimLoop = false;
+                          if ($codLang == '#tks:') {
+                               //Localiza as tarefas nas linhas seguintes:                             
+                               while(!$fimLoop){
+                                    $line = trim($arrMsg[++$i]);
+                                    if (strlen($line) == 0) continue;   
+  
+                                    if (preg_match("/^-/", $line) || preg_match("/^·/", $line)){
+                                        $arrTask[] = $line;
+                                    } else {                                                    
+                                        if (preg_match("/^#[[:alpha:]]{2,3}:/", $line)) $i--;
+                                        $str = str_split($line);
+                                        echo $line.'<br>';
+                                        foreach($str as $char) {
+                                            echo chr(ord($char)).'<br>';
+                                        }
+                                        $fimLoop = true;
+                                    }
+                               }
+                               $arrTag[$codLang] = $arrTask;
+                          } else {                              
+                               $arrPartTag = explode($codLang,$line);
+                               if (isset($arrPartTag[1]) && strlen($arrPartTag[1]) > 0) {
+                                   //O conteúdo do pseudo-código está na mesma linha.                               
+                                   $arrTag[$codLang] = $arrPartTag[1];
+                                   //if (strlen($arrTag[1]) == 0) $arrTag[$codLang] = $arrMsg[++$i];
+                               } else {
+                                   while(!$fimLoop){
+                                       $line = trim($arrMsg[++$i]);
+                                       if (strlen($line) == 0) continue;    
+                                       $arrTag[$codLang] = $line;
+                                       $fimLoop = true;
+                                   }                                                                           
+                               }
+                          }
+                        }
+                    }
+                }
+            }
+            print_r($arrTag);
             die();
 
             /*
@@ -52,5 +114,17 @@
         }
     } else {
         echo 'Não há mensagens';
+    }
+    
+    function getTask($line, $arrOut = array()){
+        if (strlen(trim($line)) == 0) return false;
+        $arrOut[] = $line;
+        return $arrOut;
+    }
+    
+    function delLinhaVazia($var){
+        $str = trim($var);
+        if (strlen($str) == 0) return false;
+        return $var;
     }
 ?>
