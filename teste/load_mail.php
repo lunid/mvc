155bbb1a6f37a74dbc9cbfc680ed58cbce896d77
Claudio <Claudio@Claudio-PC>
2014-03-20 15:18:57 -0300

    UPdate.

--- teste/load_mail.php
+++ teste/load_mail.php
@@ -3,12 +3,31 @@
     $port   = '110';
     $user   = 'project@supervip.com.br';
     $passwd = 'senha3040';
+       
+    $arrCodMap      =  array(
+      'title'       => 'tt, título',
+      'descryption' => 'ds,descrição',
+      'task'        => 'tk,tarefa',
+      'tasks'       => 'tks,tarefas',
+      'type'        => 'tp,tipo',
+      'deadline'    => 'dl,data de conclusão',
+      'tag'         => 'tags,palavra-chave, palavras-chave'        
+    );
     
-    $arrPseudoCod = array('title','tt','ds','task','tk','tasks','tks','type','tp','deadline','dl','tag');
+    foreach($arrCodMap as $key=>$value){
+        $arrCod[] = $key;
+    }
+    
+    $strCodKey      = join(',',$arrCod);
+    $strCodValue    = join(',',$arrCodMap);
+    if (strlen($strCodValue) > 0) $strCodKey .= ','.$strCodValue;
+    $arrPseudoCod = explode(',',$strCodKey);
+
+
     
     $mbox = imap_open("{{$server}:{$port}/pop3/novalidate-cert}INBOX", $user, $passwd);
     if ($mbox) {
-        $totalMsg = imap_num_msg($mbox);
+        $totalMsg = imap_num_msg($mbox);        
         for ($msgId = 1; $msgId <= $totalMsg; $msgId++) {
             $header                 = imap_header($mbox, $msgId);
             $message['subject']     = $header->subject;
@@ -19,57 +38,76 @@
             $message['body']        = imap_fetchbody($mbox,$msgId,"1"); ## GET THE BODY OF MULTI-PART MESSAGE
             if(!$message['body']) {$message['body'] = '[Nenhuma mensagem foi enviada]\n\n';}
             
-            $titulo     = utf8_decode(iconv_mime_decode($message['subject'],0,"UTF-8"));           
+            $titulo     = utf8_decode(iconv_mime_decode($message['subject'],0,"UTF-8"));               
+            
+            //Converte data para o formato Y-m-d H:i:s
+            $arrDate    = date_parse($message['date']);            
+            $dataHoraEn = $arrDate['year'].'-'.$arrDate['month'].'-'.$arrDate['day'].' '.$arrDate['hour'].':'.$arrDate['minute'].':'.$arrDate['second'];
+            
             $msg        = imap_qprint($message['body']);
             $arrMsgTk   = $arrMsg = explode("\n",$msg);                 
             //$arrMsg     = array_filter($arrMsgTk, "delLinhaVazia");
             //$arrMsg     = array_values($arrMsg);
             $arrTag     = array();
             $arrTask    = array();
+            $codKey     = '';//Índice associativo do arrTag
             
             if (is_array($arrMsg)) {
                 $tam = count($arrMsg);
   
                 for($i=0; $i < $tam; $i++) {
                     $line = trim($arrMsg[$i]);
+ 
                     foreach($arrPseudoCod as $cod) {
-                        $codLang = '#'.$cod.':';
-                        
+                        $codLang    = '#'.$cod.':';
+                        $codKey     = '';
+                        if (isset($arrCodMap[$cod])) {
+                            $codKey = $cod;
+                        } else {
+                            $codKey = checkCodMap($arrCodMap,$cod);
+                            if ($codKey === FALSE) continue;//A tag informada não existe.
+                        }
+
                         $key = strpos($line,$codLang);
                         if ($key !== false) {
                            //Encontrou uma tag na linha atual
                           //echo $codLang. $line.'...<br>';                           
                           $fimLoop = false;
-                          if ($codLang == '#tks:') {
+                          if ($codLang == '#tks:' || $codLang == '#tasks:') {
                                //Localiza as tarefas nas linhas seguintes:                             
                                while(!$fimLoop){
-                                    $line = trim($arrMsg[++$i]);
+                                    $line   = trim($arrMsg[++$i]);                                    
+                                    //$line = preg_replace("/\s\s+/", "",$line);
+
                                     if (strlen($line) == 0) continue;   
-  
-                                    if (preg_match("/^-/", $line) || preg_match("/^·/", $line)){
-                                        $arrTask[] = $line;
+                                    $char1  = ord($line[0]);//Primeiro caractere da linha
+                                    
+                                    if (preg_match("/^-/", $line) || $char1 == 183){                                            
+                                        $line   = preg_replace("/\s\s+/", "",$line);//retira espaços vazios adicionais
+                                        $line   = preg_replace("/^(-)\s?/", "",$line);//retira hífen no início da linha com ou sem espaço à direita
+                                        $line = preg_replace("/[^\x01-\x7F]/","", $line);//remove qualquer caractere não ASCII
+ 
+                                        $arrTask[]  = $line;
                                     } else {                                                    
-                                        if (preg_match("/^#[[:alpha:]]{2,3}:/", $line)) $i--;
-                                        $str = str_split($line);
-                                        echo $line.'<br>';
-                                        foreach($str as $char) {
-                                            echo chr(ord($char)).'<br>';
-                                        }
+                                        if (preg_match("/^#[[:alpha:]]{2,3}:/", $line)) $i--;//Se for uma tag (#..:) volta um item no loop                                       
                                         $fimLoop = true;
                                     }
                                }
-                               $arrTag[$codLang] = $arrTask;
-                          } else {                              
+                               $arrTag[$codKey] = $arrTask;
+                          } else { 
+                               
                                $arrPartTag = explode($codLang,$line);
                                if (isset($arrPartTag[1]) && strlen($arrPartTag[1]) > 0) {
                                    //O conteúdo do pseudo-código está na mesma linha.                               
-                                   $arrTag[$codLang] = $arrPartTag[1];
+                                   $arrTag[$codKey] = $arrPartTag[1];
                                    //if (strlen($arrTag[1]) == 0) $arrTag[$codLang] = $arrMsg[++$i];
                                } else {
+                                   $i++;//Avança uma linha.
                                    while(!$fimLoop){
-                                       $line = trim($arrMsg[++$i]);
-                                       if (strlen($line) == 0) continue;    
-                                       $arrTag[$codLang] = $line;
+                                       //Localiza a próxima linha com texto.                                       
+                                       $line = trim($arrMsg[++$i]);                                      
+                                       if (strlen($line) == 0) continue; //Ignora linha vazia  
+                                       $arrTag[$codKey] = $line;
                                        $fimLoop = true;
                                    }                                                                           
                                }
@@ -78,6 +116,32 @@
                     }
                 }
             }
+            
+            $type           = 'chore';
+            $deadline       = '';
+            $tags           = '';
+            $memo           = '';
+            $descryption    = '';
+            
+            if (isset($arrTag['type'])) $type = $arrTag['type'];
+            if (isset($arrTag['descryption'])) $descryption = $arrTag['descryption'];
+            if (isset($arrTag['deadline'])) $deadline = $arrTag['deadline'];
+            if (isset($arrTag['tag'])) $tags = $arrTag['tag'];
+            
+            $author = $message['fromaddress'];
+            $to     = $message['toaddress'];
+            $copy   = $message['ccaddress'];           
+                                
+            echo "Data/hora:{$dataHoraEn} <br/>";
+            echo "Tipo:{$type} <br/>";
+            echo "Descrição:{$descryption} <br/>";
+            echo "Autor:{$author} <br/>";
+            echo "De:{$author} para: {$to}<br/>";
+            echo "Cópia para: {$copy}<br/>";
+            echo "Prazo/conclusão:{$deadline}<br/>";
+            echo "Tags:{$tags}<br/>";
+            echo "Memo:{$memo}<br/>";
+            
             print_r($arrTag);
             die();
 
@@ -114,6 +178,18 @@
         }
     } else {
         echo 'Não há mensagens';
+    }
+    
+    function checkCodMap($arrRoadMap,$codSearch){
+        if (is_array($arrRoadMap) && strlen($codSearch) > 0) {
+            foreach($arrRoadMap as $codKey => $cod) {
+                $key = strpos($cod,$codSearch);
+                if ($key !== false) {
+                    return $codKey;
+                }
+            }
+        }
+        return false;
     }
     
     function getTask($line, $arrOut = array()){
